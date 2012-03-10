600ff6feec6303376fc83e325689275178dff560
unknown <tom@tom-PC.(none)>
2012-03-10 17:55:12 +0100

    Signed-off-by: Tom <tom@tom-PC.(none)> modification du temps vias la mollet

--- xpi/chrome/content/timesheets.js
+++ xpi/chrome/content/timesheets.js
@@ -76,7 +76,7 @@ function startup() {
   gDialog.content          = document.getElementById("content");
   gDialog.segmentTemplate  = document.getElementById("segmentTemplate");
   gDialog.xblFormTemplate  = document.getElementById("xblFormTemplate");
-  gDialog.timeSegments     = document.getElementById("timeSegments");
+  //gDialog.timeSegments     = document.getElementById("timeSegments");
   gDialog.timeContainer    = document.getElementById("container");
 
   // thumbnails
@@ -102,162 +102,237 @@ function startup() {
   loadMediaFiles();
 }
 
-function consoleLog(message) {
-  gDialog.console.value += message + "\n" ;
-}
-
-// load media source and waveform
-function loadMediaFiles(aForceReload) {
-  var baseURL = gDialog.mediaBaseURI.value;
-  gTimeController.media.src    = baseURL + gDialog.mediaSource.value;
-  gTimeController.waveform.src = baseURL + gDialog.mediaWaveform.value;
-  // gWaveform.src                = baseURL + gDialog.mediaWaveform.value;
-  if (aForceReload) {
-    gTimeController.waveform.load();
-    // gWaveform.load();
-  }
-}
-
 // time segments
 function newDataForm(begin, end, cpt) {
- // var form = gDialog.xblFormTemplate.cloneNode(true);
- // form.removeAttribute("id");
- // form.removeAttribute("hidden");
   var dataForm = document.createElement("dataform");
   var vbox = document.getElementById("content");
   vbox.appendChild(dataForm);
   consoleLog(begin);
-  //dataForm.begin = begin;
-  //dataForm.end   = end;
-  dataForm.setAttribute("id","df"+cpt);
+  dataForm.setAttribute("id","df:"+cpt);
   var cont = document.getElementById("content");
   for(var i = 0; i < cont.childNodes.length; i++)
   cont.childNodes[i].style.visibility="hidden";
-  dataForm.setAttribute("style", "visibility=visible")
-  //dataForm.max = gTimeContainer.duration;
+  dataForm.setAttribute("style", "visibility=visible");
   return dataForm;
 }
 function newSegment() {
   
-  var sel   = gTimeController.selection;
-  var begin = Math.round(sel.begin * 100) / 100;
-  var end   = Math.round(sel.end   * 100) / 100;
+  //alert("test");
+  
+  //var sel   = gTimeController.selection;
+  //var begin = Math.round(sel.begin * 100) / 100;
+  //var end   = Math.round(sel.end   * 100) / 100;
+  
+  var begin = document.getElementById("mediaTime1").time;
+  var end = document.getElementById("mediaTime2").time;
+  
+  var wave = document.getElementById("wave");
+ // alert(wave.currentTime);
+  
+ // alert(wave.currentTime);
+  
+  //alert(sel);
+  //alert(begin);
+  //alert(end);
+  
   if (begin == end)
     end = Infinity;
 
   cptTimeSegment++;
 	
+	
   // quick and dirty way
-  gTimeSegments.push(new timeSegment(begin, end, cptTimeSegment));
-
-  // TODO: let's be serious...
-  // gTimeContainer will take the begin/end values from its 'controls' element
-  // but passing these begin/end values manually would make sense.
-  // Passing a 'segmentControls' element would make sense, too.
-  var form = newDataForm(begin, end, cptTimeSegment);
-  //var form = document.createElement("dataform");
-  //setTimeout(function() { // XXX
-  //  form.begin = begin;
-  //  form.end   = end;
-  //}, 0);
-  //gDialog.content.appendChild(form);
+  //gTimeSegments.push(new timeSegment(begin, end, cptTimeSegment));
 
   gTimeContainer.add(begin, end, cptTimeSegment);
-  //gTimeContainer.draw();
-}
-function delSegment(timeSegment) {
-  var i = gTimeSegments.indexOf(timeSegment);
-  if (i >= 0) {
-    var ctrl = gTimeSegments[i].controls;
-    var blck = gTimeSegments[i].block;
-    ctrl.main.parentNode.removeChild(ctrl.main);
-    blck.main.parentNode.removeChild(blck.main);
-    gTimeSegments.splice(i, 1);
-  }
-}
-function sortSegments() {
-  var swap, data1, data2;
-  do { // there are few items, we can do a lazy bubble sort
-    swap = false;
-    for (var i = 0; i < gTimeSegments.length - 1; i++) {
-      //consoleLog("order #" + i);
-      var curr = gTimeSegments[i];
-      var next = gTimeSegments[i+1];
-      if (curr.begin > next.begin) { // swap these two items
-        //consoleLog("swap #" + i);
-        data1 = curr.controls.data.value;
-        data2 = next.controls.data.value;
-        gDialog.content.insertBefore(next.controls.main, curr.controls.main);
-        gDialog.timeSegments.insertBefore(next.block.main, curr.block.main);
-        curr.controls.data.value = data1;
-        next.controls.data.value = data2;
-        gTimeSegments[i]   = next;
-        gTimeSegments[i+1] = curr;
-        swap = true;
-      }
-    }
-  } while(swap);
+  //var form = newDataForm(begin, end, cptTimeSegment);
+  //var thumb = new segmentThumb(begin, end, cptTimeSegment, gDialog.sidebarLeft);
+  
+  //alert(wave.currentTime);
+  
+  document.getElementById("mediaTime1").time = begin;
+  wave.currentTime = document.getElementById("mediaTime1").time;
 }
 
-// redraw time blocks
-function redrawSegmentBlocks(event) {
- // consoleLog("<" + event.target.nodeName + "> redraw");
-  //var begin = event.target.begin; // == gTimeController.begin
-  //var end   = event.target.end;   // == gTimeController.end
-  var begin = event.begin; // == waveform.begin
-  var end   = event.end;   // == waveform.end
-  //alert(begin+" - "+end);
-  //consoleLog(begin + " → " + end);
-  for (var i = 0; i < gTimeSegments.length; i++) {
-    gTimeSegments[i].block.draw(begin, end);
+// thumbnails
+function segmentThumb(begin, end, cpt, sidebar) {
+	
+  thumb = document.createElement("slide");
+  sidebar.appendChild(thumb);
+  thumb.setAttribute("id","thumb:"+cpt);
+  
+  
+  var slideCurrent;
+  var slide = document.getElementById("sidebar-left");
+	
+  for(var i = (slide.childNodes.length)-1; i >= 0; i--)
+  {
+	slideCurrent = slide.childNodes[i];
+	if(slideCurrent.deb > begin)
+	{
+	  slide.insertBefore(thumb, slide.childNodes[i]);
+	}
   }
-  gTimeContainer.draw(begin, end);
+	
+  thumb.max = cpt;
+  thumb.begin = begin;
+  thumb.end = end;
+	
+  thumb.addEventListener('click',onClick,false);
+	
+  return thumb;
 }
-function computeTimeNodes() { // XXX
-  sortSegments();
-  if (!gDialog.timeContainer) return;
-  var timeContainer = gDialog.timeContainer.value;
-  //consoleLog(timeContainer);
-  for (var i = 0; i < gTimeSegments.length - 1; i++) {
-    var out;
-    var end = gTimeSegments[i].end;
-    //if (!end.length) end = Infinity;
-    //consoleLog("end = " + end);
-    if ((timeContainer == "par") || (end < gTimeSegments[i+1].begin))
-      out = gTimeSegments[i].end;
-    else
-      out = gTimeSegments[i+1].begin;
-    gTimeSegments[i].time_in = gTimeSegments[i].begin;
-    gTimeSegments[i].time_out = out;
-    gTimeSegments[i].block.update();
+function timeupdatebegin(event)
+{
+  waveform.currentTime = mediaTime.time;
+  drawOnChange(mediaTime.time,mediaTimeEnd.time);
+}
+function onClick(event) {
+			  
+	var elm = event.target;
+	var idTemp = elm.getAttribute("id",0);
+	var id2 = idTemp.split(":");
+	var id = id2[1];
+	
+	//Affichage du Dataform
+	var df = document.getElementById("df:"+id);
+	var cont = document.getElementById("content");
+	for(var i = 0; i < cont.childNodes.length; i++)
+	  cont.childNodes[i].style.visibility="hidden";
+	df.style.visibility="visible";
+	
+	//Récupération du timeNode et Slide
+	var slide = document.getElementById("thumb:"+id);
+	var timeNode = document.getElementById("timeNode:"+id);
+	
+	//On affiche en rouge uniquement le timeNode sélectionné
+	var parentTimeNode = timeNode.parentNode;
+	var i = 0;
+	for(i; i < parentTimeNode.childNodes.length; i++)
+	{
+	  parentTimeNode.childNodes[i].className = "unselected";
+	}
+	//alert(timeNode.className);
+	timeNode.className = "selected";
+	//alert(timeNode.getAttribute("id",0));
+	//alert(timeNode.className);
+	
+	//On affiche en rouge uniquement la slide sélectionnée
+	//var hbox = document.getAnonymousElementByAttribute(slide, "anonid", "formslide");
+	var parentThumb = slide.parentNode;
+	var j = 0;
+	for(j; j < parentThumb.childNodes.length; j++)
+	{
+	  //var hbox2 = document.getAnonymousElementByAttribute(parentThumb.childNodes[j], "anonid", "formslide");
+	  parentThumb.childNodes[j].className = "unselected";
+	}
+	slide.className = "selected";
+	
+	//On redessine sur la waveform
+	var waveform = document.getElementById("wave");
+	
+	var mediaTime = document.getElementById("mediaTime1");
+	var mediaTime2 = document.getElementById("mediaTime2");
+	
+	var time1 = document.getElementById("time1");
+	var time2 = document.getElementById("time2");
+	
+	var left  = Math.max(slide.deb,  waveform.begin);
+	var right = Math.min(slide.fin, waveform.end);
+
+	//alert(mediaTime.onTimeUpdate);
+	//mediaTime.removeEventListener("timeupdate",  timeupdatebegin, false);
+	//mediaTime.onTimeUpdate = null;
+	//alert(mediaTime.timeupdate);
+	//alert(mediaTime.ontimeupdate);
+	//alert(mediaTime.onTimeUpdate);
+	//for (i in mediaTime) {
+    //  alert(i);
+    //}
+	//alert(mediaTime.change);
+	//alert("test1");
+	//mediaTime.removeEventListener("change", timeupdatebegin, false);
+	//alert("test2");
+	mediaTime.time = slide.deb;
+	//mediaTime.addEventListener("timeupdate", timeupdatebegin, false);
+	//alert("debutslide"+slide.deb);
+	//alert("debut"+mediaTime.time);
+	mediaTime2.time = slide.fin;
+	//alert("finslide"+slide.fin);
+	//alert("fin"+mediaTime2.time);
+	
+	if (left >= right) {
+	  style.display = "none";
+	} else {
+	  var duration = waveform.end - waveform.begin;
+	  var ratio = (right - left) / duration;
+	  var offset = (left - waveform.begin) / duration;
+	  time1.style.left  = (offset * 100) + "%";
+	  time2.style.left  = (offset * 100) + "%";
+	  time2.style.width = (ratio  * 100) + "%";
+	}
+	
   }
-  //redrawSegmentBlocks(gTimeController.begin, gTimeController.end);
-  redrawSegmentBlocks();
+// convert seconds (float) to a time string (0:00 or 0:00:00)
+function time2hms(time) {
+  if (isNaN(time) || time >= Infinity || time == 0)
+    return "";
+  var seconds = Math.round(time * 100) / 100;
+  var sec = seconds % 60;
+  sec = Math.round(sec * 100) / 100;
+  var str = sec;
+  if (sec < 10)
+    str = "0" + str;
+  var minutes = Math.floor(seconds / 60);
+  var min = minutes % 60;
+  str = min + ":" + str;
+  if (!gMediaPlayer || (gMediaPlayer.duration < 3600)) return str;
+  if (min < 10)
+    str = "0" + str;
+  var h = Math.floor(minutes / 60);
+  str = h + ":" + str;
+  return str;
+}
+function hms2time(hms) {
+  var seconds = 0;
+  var tmp = hms.split(":");
+  for (var i = 0; i < tmp.length; i++)
+    seconds = (seconds * 60) + parseFloat(tmp[i]);
+  return seconds;
 }
 
+// test
+function scrollTest(event) {
+  //console.log(event);
+  //console.log(event.detail);
+  var scroll = document.getElementById("scrollTest");
+  //consoleLog(scroll.scrollLeft);
+}
 // main 'timeSegment' object
 function timeSegment(begin, end, cpt) {
 
   //consoleLog("new: " + begin + " → " + end);
   const self = this;
 
-  this.begin = begin;
-  this.end   = end;
+  //this.begin = begin;
+  //this.end   = end;
 
-  this.time_in  = begin;
-  this.time_out = end;
+  //this.time_in  = begin;
+  //this.time_out = end;
   
   // append a XUL groupbox in #content
   //var controls = new segmentControls(this, begin, end);
   //gDialog.content.appendChild(controls.main);
 
   // append an HTML segment in #timeSegments
-  var block = new segmentBlock(this, begin, end);
-  gDialog.timeSegments.appendChild(block.main);
-  gDialog.timeSegments.setAttribute("width","15%");
+  //var block = new segmentBlock(this, begin, end);
+  //gDialog.timeSegments.appendChild(block.main);
+  //gDialog.timeSegments.setAttribute("width","15%");
 
   // append a thumbnail in #sidebar-left
-  var thumb = new segmentThumb(begin, end, cpt, gDialog.sidebarLeft);
+  alert("test");
+	
+  //var thumb = new segmentThumb(begin, end, cpt, gDialog.sidebarLeft);
   //gDialog.sidebarLeft.appendChild(thumb);
 
   this.update = function(aBegin, aEnd) {
@@ -334,7 +409,83 @@ function timeSegment(begin, end, cpt) {
   this.block    = block;
    
 }
+function consoleLog(message) {
+  gDialog.console.value += message + "\n" ;
+}
+// load media source and waveform
+function loadMediaFiles(aForceReload) {
+  var baseURL = gDialog.mediaBaseURI.value;
+  gTimeController.media.src    = baseURL + gDialog.mediaSource.value;
+  gTimeController.waveform.src = baseURL + gDialog.mediaWaveform.value;
+  // gWaveform.src                = baseURL + gDialog.mediaWaveform.value;
+  if (aForceReload) {
+    gTimeController.waveform.load();
+    // gWaveform.load();
+  }
+}
+function delSegment(timeSegment) {
+  /*var i = gTimeSegments.indexOf(timeSegment);
+  if (i >= 0) {
+    var ctrl = gTimeSegments[i].controls;
+    var blck = gTimeSegments[i].block;
+    ctrl.main.parentNode.removeChild(ctrl.main);
+    blck.main.parentNode.removeChild(blck.main);
+    gTimeSegments.splice(i, 1);
+  }*/
+}
+function sortSegments() {
+  /*var swap, data1, data2;
+  do { // there are few items, we can do a lazy bubble sort
+    swap = false;
+    for (var i = 0; i < gTimeSegments.length - 1; i++) {
+      //consoleLog("order #" + i);
+      var curr = gTimeSegments[i];
+      var next = gTimeSegments[i+1];
+      if (curr.begin > next.begin) { // swap these two items
+        //consoleLog("swap #" + i);
+        data1 = curr.controls.data.value;
+        data2 = next.controls.data.value;
+        gDialog.content.insertBefore(next.controls.main, curr.controls.main);
+        gDialog.timeSegments.insertBefore(next.block.main, curr.block.main);
+        curr.controls.data.value = data1;
+        next.controls.data.value = data2;
+        gTimeSegments[i]   = next;
+        gTimeSegments[i+1] = curr;
+        swap = true;
+      }
+    }
+  } while(swap);*/
+}
+// <groupbox> control for the #content container
+function segmentControls(parent, begin, end) {
+  var self = this;
 
+  // TODO design an XBL instead of this ugly thing
+  this.main  = gDialog.segmentTemplate.cloneNode(true);
+  this.begin = this.main.getElementsByTagName("textbox").item(0);
+  this.end   = this.main.getElementsByTagName("textbox").item(1);
+  this.data  = this.main.getElementsByTagName("textbox").item(2);
+
+  this.main.removeAttribute("id");
+  this.main.removeAttribute("hidden");
+  this.begin.setAttribute("value", time2hms(begin));
+  this.end.setAttribute("value", time2hms(end));
+
+  function hmsFormat() {
+    if (/^[0-9]*[\.]{0,1}[0-9]*$/.test(this.value))
+      this.value = time2hms(this.value);
+  }
+  this.begin.onchange = hmsFormat;
+  this.end.onchange   = hmsFormat;
+
+  this.focus = function () {
+    //consoleLog(self.data.value);
+    self.data.focus();
+  };
+  this.blur = function () {
+    self.data.blur();
+  };
+}
 // <hbox> block for the #timeSegments container
 function segmentBlock(parent, begin, end) {
   var self = this;
@@ -400,121 +551,40 @@ function segmentBlock(parent, begin, end) {
     self.begin.addEventListener("mousemove", onResize, false);
   }, false);
 }
-
-// <groupbox> control for the #content container
-function segmentControls(parent, begin, end) {
-  var self = this;
-
-  // TODO design an XBL instead of this ugly thing
-  this.main  = gDialog.segmentTemplate.cloneNode(true);
-  this.begin = this.main.getElementsByTagName("textbox").item(0);
-  this.end   = this.main.getElementsByTagName("textbox").item(1);
-  this.data  = this.main.getElementsByTagName("textbox").item(2);
-
-  this.main.removeAttribute("id");
-  this.main.removeAttribute("hidden");
-  this.begin.setAttribute("value", time2hms(begin));
-  this.end.setAttribute("value", time2hms(end));
-
-  function hmsFormat() {
-    if (/^[0-9]*[\.]{0,1}[0-9]*$/.test(this.value))
-      this.value = time2hms(this.value);
-  }
-  this.begin.onchange = hmsFormat;
-  this.end.onchange   = hmsFormat;
-
-  this.focus = function () {
-    //consoleLog(self.data.value);
-    self.data.focus();
-  };
-  this.blur = function () {
-    self.data.blur();
-  };
+// redraw time blocks
+function redrawSegmentBlocks(event) {
+ // consoleLog("<" + event.target.nodeName + "> redraw");
+  //var begin = event.target.begin; // == gTimeController.begin
+  //var end   = event.target.end;   // == gTimeController.end
+  var begin = event.begin; // == waveform.begin
+  var end   = event.end;   // == waveform.end
+  //alert(begin+" - "+end);
+  //consoleLog(begin + " → " + end);
+  /*for (var i = 0; i < gTimeSegments.length; i++) {
+    gTimeSegments[i].block.draw(begin, end);
+  }*/
+  gTimeContainer.draw(begin, end);
 }
-
-// thumbnails
-function segmentThumb(begin, end, cpt, sidebar) {
-  
-  // TODO fill with <canvas> and transition class
-  /*this.main  = document.createElementNS(xulNS, "hbox"); // main block
-  
-  this.focus = function () {
-    self.main.className = "active";
-  };
-  this.blur = function () {
-    self.main.removeAttribute("class");
-  };
-  var thumb = document.createElement("slide");
-  thumb.setAttribute("id","thumb"+cpt);
-    this.main.appendChild(thumb);*/
-	
-	
-  thumb = document.createElement("slide");
-  sidebar.appendChild(thumb);
-  thumb.setAttribute("id","thumb"+cpt);
-  
-  var slideCurrent;
-  var slide = document.getElementById("sidebar-left");
-	
-  for(var i = (slide.childNodes.length)-1; i >= 0; i--)
-  {
-	slideCurrent = slide.childNodes[i];
-	if(slideCurrent.deb > begin)
-	{
-	  slide.insertBefore(thumb, slide.childNodes[i]);
-	}
-  }
-	
-  thumb.max = cpt;
-  thumb.begin = begin;
-  thumb.end = end;
-	
-  /*this.focus = function () {
-    self.main.className = "active";
-  };
-  this.blur = function () {
-    self.main.removeAttribute("class");
-  };*/
-  
-  
-  return thumb;
+function computeTimeNodes() { // XXX
+  sortSegments();
+  if (!gDialog.timeContainer) return;
+  var timeContainer = gDialog.timeContainer.value;
+  //consoleLog(timeContainer);
+  /*for (var i = 0; i < gTimeSegments.length - 1; i++) {
+    var out;
+    var end = gTimeSegments[i].end;
+    //if (!end.length) end = Infinity;
+    //consoleLog("end = " + end);
+    if ((timeContainer == "par") || (end < gTimeSegments[i+1].begin))
+      out = gTimeSegments[i].end;
+    else
+      out = gTimeSegments[i+1].begin;
+    gTimeSegments[i].time_in = gTimeSegments[i].begin;
+    gTimeSegments[i].time_out = out;
+    gTimeSegments[i].block.update();
+  }*/
+  //redrawSegmentBlocks(gTimeController.begin, gTimeController.end);
+  redrawSegmentBlocks();
 }
-
-// convert seconds (float) to a time string (0:00 or 0:00:00)
-function time2hms(time) {
-  if (isNaN(time) || time >= Infinity || time == 0)
-    return "";
-  var seconds = Math.round(time * 100) / 100;
-  var sec = seconds % 60;
-  sec = Math.round(sec * 100) / 100;
-  var str = sec;
-  if (sec < 10)
-    str = "0" + str;
-  var minutes = Math.floor(seconds / 60);
-  var min = minutes % 60;
-  str = min + ":" + str;
-  if (!gMediaPlayer || (gMediaPlayer.duration < 3600)) return str;
-  if (min < 10)
-    str = "0" + str;
-  var h = Math.floor(minutes / 60);
-  str = h + ":" + str;
-  return str;
-}
-function hms2time(hms) {
-  var seconds = 0;
-  var tmp = hms.split(":");
-  for (var i = 0; i < tmp.length; i++)
-    seconds = (seconds * 60) + parseFloat(tmp[i]);
-  return seconds;
-}
-
-// test
-function scrollTest(event) {
-  //console.log(event);
-  //console.log(event.detail);
-  var scroll = document.getElementById("scrollTest");
-  //consoleLog(scroll.scrollLeft);
-}
-
 //document.getElementById("scrollTest").addEventListener("scroll", scrollTest, false);
 
